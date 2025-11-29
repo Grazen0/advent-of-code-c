688e556e7e92e3f9206ba0091cf6a9c20441791d
José Daniel Grayson <josedanielgrayson@proton.me>
2025-11-29 17:26:20 -0500

    feat: add 2024 day 2 partial solution

---
+++ src/2024/day_02_1.c
@@ -0,0 +1,50 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int sum = 0;
+
+    char *line = NULL;
+
+    while (true) {
+        size_t alloc_size = 0;
+        const ssize_t len = getline(&line, &alloc_size, stdin);
+
+        if (len == -1)
+            break;
+
+        FILE *const fline = fmemopen(line, len, "r");
+
+        int dir = 0;
+        int n = -1;
+        int last_n = -1;
+
+        while (fscanf(fline, "%i", &n) != EOF) {
+            if (last_n != -1) {
+                if (dir == 0)
+                    dir = n - last_n;
+
+                if (dir > 0 ? n < last_n : n > last_n)
+                    goto skip;
+
+                const int diff = abs(last_n - n);
+                if (diff < 1 || diff > 3)
+                    goto skip;
+            }
+
+            last_n = n;
+        }
+
+        ++sum;
+skip:
+        fclose(fline);
+    }
+
+    free(line);
+
+    printf("%i\n", sum);
+    return 0;
+}
---
+++ src/2024/day_02_2.c
@@ -0,0 +1,62 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int sum = 0;
+
+    char *line = NULL;
+
+    while (true) {
+        size_t alloc_size = 0;
+        const ssize_t len = getline(&line, &alloc_size, stdin);
+
+        if (len == -1)
+            break;
+
+        FILE *const fline = fmemopen(line, len, "r");
+
+        int dir = 0;
+        int n = -1;
+        int last_n = -1;
+        bool removed_level = false;
+
+        while (fscanf(fline, "%i", &n) != EOF) {
+            if (last_n == -1) {
+                last_n = n;
+                continue;
+            }
+
+            const int prev_dir = dir;
+            if (dir == 0)
+                dir = n - last_n;
+
+            const int diff = abs(n - last_n);
+
+            const bool dir_check = dir > 0 ? n > last_n : n < last_n;
+            const bool diff_check = diff >= 1 && diff <= 3;
+
+            if (!dir_check || !diff_check) {
+                if (removed_level)
+                    goto skip;
+
+                dir = prev_dir;
+                removed_level = true;
+                continue;
+            }
+
+            last_n = n;
+        }
+
+        ++sum;
+skip:
+        fclose(fline);
+    }
+
+    free(line);
+
+    printf("%i\n", sum);
+    return 0;
+}
